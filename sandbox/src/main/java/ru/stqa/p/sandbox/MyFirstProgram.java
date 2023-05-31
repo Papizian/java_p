dbc5f70c05e79762d82d64f9bc4d4738955502d9
dreybot_164 <95182154+Papizian@users.noreply.github.com>
2023-05-31 23:43:54 +0400

    formatted code

--- sandbox/src/main/java/ru/stqa/p/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/p/sandbox/MyFirstProgram.java
@@ -2,6 +2,6 @@ package ru.stqa.p.sandbox;
 
 public class MyFirstProgram {
     public static void main(String[] args) {
-    System.out.println("HEllo");
-            }
+        System.out.println("HEllo");
+    }
 }
